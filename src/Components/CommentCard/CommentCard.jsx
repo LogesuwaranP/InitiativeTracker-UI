fb12bd284527b59b54b38b4e58504441509a874d
LogesuwaranP <logesuwaran005@gmail.com>
2023-07-16 20:23:29 +0530

    Detailed-view

---
+++ src/Components/CommentCard/CommentCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./CommentCard.css"
+const CommentCard = () => {
+  return (
+    <div className='comments-container'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. !</p>
+        <div className='comments-lable'>
+            <div>
+                <img src='https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png'/>
+                <p>Logesuwaran</p>
+            </div>
+            <p>7/16/2023</p>
+
+        </div>
+              
+    </div>
+  )
+}
+
+export default CommentCard
